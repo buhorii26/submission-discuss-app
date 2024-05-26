ed571d05c2ddf45397b594c8cca67290b714c927
buhorii26 <buhorics@gmail.com>
2024-05-26 13:39:01 +0900

    make test passed

--- src/states/threads/reducer.js
+++ src/states/threads/reducer.js
@@ -4,7 +4,7 @@ function threadsRecuder(threads = [], action = {}) {
   switch (action.type) {
     case ActionType.RECEIVE_THREADS:
       return action.payload.threads;
-    case ActionType.ADD_THREADS:
+    case ActionType.ADD_THREAD:
       return [action.payload.thread, ...threads];
     case ActionType.UP_VOTE:
       return threads.map((thread) => {
